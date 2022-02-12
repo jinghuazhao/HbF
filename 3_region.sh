91c510009a5e701f3c22db48f3ce17824f1719e1
Jing Hua Zhao <jinghuazhao@hotmail.com>
2022-02-12 16:18:05 +0000

    HbF

--- 3_region.sh
+++ 3_region.sh
@@ -4,31 +4,7 @@ export HbF=${HOME}/COVID-19/HbF
 export pgwas=~/rds/results/public/proteomics
 export M=1e6
 
-#ARIC, https://sites.cscc.unc.edu/aric/, build 38
-export ARIC=${pgwas}/ARIC
-ls ${ARIC}/EA/*gz | parallel -C' ' -j15 'tabix -S1 -s1 -b2 -e2 -f {}'
-cat <(awk -v OFS="\t" '{print "rsid","snpid","Gene","Somamer","Uniprot","Symbol",$0}' ${ARIC}/glm.linear.hdr) \
-    <(
-       sed '1d' ${HbF}/work/hbf_hits.txt | cut -f1,3,4,12,13 | grep -v -w NA | sed 's/, /;/g' | \
-       while read -r chr pos rsid snpid gene
-       do
-          export chr=${chr}
-          export pos=${pos}
-          export rsid=${rsid}
-          export snpid=${snpid}
-          export gene=${gene}
-          export region=$(awk -vchr=${chr} -vpos=${pos} -vM=${M} 'BEGIN{print chr":"pos-M"-"pos+M}')
-          sed '1d' ${ARIC}/seqid.txt | cut -f1-3 | tr '\t' ' ' | \
-          parallel -C' ' -j15 --env ARIC --env rsid --env snpid --env gene --env region '
-             tabix ${ARIC}/EA/{1}.PHENO1.glm.linear.gz ${region} | \
-             awk -v rsid=${rsid} -v snpid=${snpid} -v gene=${gene} -v somamer={1} -v uniprot={2} -v symbol={3} -v OFS="\t" "
-                 \$13<=1e-5{print rsid,snpid,gene,somamer,uniprot,symbol,\$0}
-             "
-          '
-        done
-      ) > ${HbF}/work/ARIC.tsv
-
-#AGES
+#1. AGES
 export AGES=${pgwas}/AGES
 cd ${AGES}
 (
@@ -55,14 +31,12 @@ mv excluded/loc* excluded/Olink-* excluded/README.* excluded/AGES.hdr .
 rm -rf ${AGES}/excluded
 ls Olink-INF | parallel -C' ' 'rm -rf Olink-INF/{}; ln -fs {} Olink-INF/{}'
 cd -
-
 R --no-save <<END
   HbF <- Sys.getenv("HbF")
   stables <- "https://static-content.springer.com/esm/art%3A10.1038%2Fs41467-021-27850-z/MediaObjects/41467_2021_27850_MOESM18_ESM.xlsx"
   st16 <- openxlsx::read.xlsx(stables, sheet=1, colNames=TRUE, skipEmptyRows=TRUE, startRow=3)
   write.table(st16,file=file.path(HbF,"work","AGES.txt"),col.names=FALSE,quote=FALSE,row.names=FALSE,sep="\t")
 END
-
 cat <(awk -v OFS="\t" '{print "rsid","snpid","Gene","Somamer","GCST","Symbol",$0}' ${AGES}/AGES.hdr) \
     <(
        sed '1d' ${HbF}/work/hbf_hits.txt | cut -f1,2,4,11,13 | grep -v -w NA | sed 's/, /;/g' | \
@@ -84,7 +58,31 @@ cat <(awk -v OFS="\t" '{print "rsid","snpid","Gene","Somamer","GCST","Symbol",$0
        done
      ) > ${HbF}/work/AGES.tsv
 
-#deCODE
+#2. ARIC, https://sites.cscc.unc.edu/aric/, build 38
+export ARIC=${pgwas}/ARIC
+ls ${ARIC}/EA/*gz | parallel -C' ' -j15 'tabix -S1 -s1 -b2 -e2 -f {}'
+cat <(awk -v OFS="\t" '{print "rsid","snpid","Gene","Somamer","Uniprot","Symbol",$0}' ${ARIC}/glm.linear.hdr) \
+    <(
+       sed '1d' ${HbF}/work/hbf_hits.txt | cut -f1,3,4,12,13 | grep -v -w NA | sed 's/, /;/g' | \
+       while read -r chr pos rsid snpid gene
+       do
+          export chr=${chr}
+          export pos=${pos}
+          export rsid=${rsid}
+          export snpid=${snpid}
+          export gene=${gene}
+          export region=$(awk -vchr=${chr} -vpos=${pos} -vM=${M} 'BEGIN{print chr":"pos-M"-"pos+M}')
+          sed '1d' ${ARIC}/seqid.txt | cut -f1-3 | tr '\t' ' ' | \
+          parallel -C' ' -j15 --env ARIC --env rsid --env snpid --env gene --env region '
+             tabix ${ARIC}/EA/{1}.PHENO1.glm.linear.gz ${region} | \
+             awk -v rsid=${rsid} -v snpid=${snpid} -v gene=${gene} -v somamer={1} -v uniprot={2} -v symbol={3} -v OFS="\t" "
+                 \$13<=1e-5{print rsid,snpid,gene,somamer,uniprot,symbol,\$0}
+             "
+          '
+        done
+      ) > ${HbF}/work/ARIC.tsv
+
+#3. deCODE
 export deCODE=${pgwas}/deCODE
 cat <(awk -v OFS="\t" '{print "rsid","snpid","Gene","id",$0}' ${deCODE}/deCODE.hdr) \
     <(
@@ -105,7 +103,7 @@ cat <(awk -v OFS="\t" '{print "rsid","snpid","Gene","id",$0}' ${deCODE}/deCODE.h
        done
      ) > ${HbF}/work/deCODE.tsv
 
-#Fenland
+#4. Fenland
 export Fenland=${pgwas}/Fenland
 cat <(awk -v OFS="\t" '{print "rsid","snpid","Gene",$0}' ${Fenland}/all.grch37.tabix.hdr) \
     <(
@@ -130,7 +128,7 @@ Rscript -e '
   HbF <- Sys.getenv("HbF")
   stables <- "https://static-content.springer.com/esm/art%3A10.1038%2Fs42255-020-00287-2/MediaObjects/42255_2020_287_MOESM3_ESM.xlsx"
   st1 <- openxlsx::read.xlsx(stables, sheet=1, colNames=TRUE, skipEmptyRows=TRUE, cols=c(1:23), rows=c(3:95))
-  write.table(st1[c("UniProt.ID","Gene","Short_annotation")],file=file.path(HbF,"work","scallop_cvd1.txt"),col.names=FALSE,quote=FALSE,row.names=FALSE)
+  write.table(st1[c("UniProt.ID","Gene","Short_annotation")],file=file.path(HbF,"work","scallop-cvd1.txt"),col.names=FALSE,quote=FALSE,row.names=FALSE)
 '
 cat <(awk -v OFS="\t" '{print "rsid","snpid","Gene","Somamer","Symbol","Prot",$0}' ${AGES}/AGES.hdr) \
     <(
@@ -152,7 +150,7 @@ cat <(awk -v OFS="\t" '{print "rsid","snpid","Gene","Somamer","Symbol","Prot",$0
        done
      ) > ${HbF}/work/scallop-cvd1.tsv
 
-#LBC1936
+#5. LBC1936
 export LBC1936=${pgwas}/LBC1936
 Rscript -e '
   options(width=200)
@@ -185,8 +183,7 @@ cat <(awk -v OFS="\t" '{print "rsid","snpid","Gene","UniProt","Symbol","Prot",$0
        done
      ) > ${HbF}/work/LBC1938.tsv
 
-
-#INTERVAL
+#6. INTERVAL
 export INTERVAL=~/rds/results/public/proteomics/somalogic/sun_2018/raw_results/meta
 cat <(gunzip -c ${INTERVAL}/BACH2.12756.3.3/BACH2.12756.3.3_chrom_6_meta_1.tbl.gz | head -1 |
       awk -v OFS="\t" '{print "rsid","snpid","Gene","id",$0}') \
@@ -208,11 +205,9 @@ cat <(gunzip -c ${INTERVAL}/BACH2.12756.3.3/BACH2.12756.3.3_chrom_6_meta_1.tbl.g
        done
      ) > ${HbF}/work/INTERVAL.tsv
 
-#GTEx, eQTL Catalog
+#7. GTEx, eQTL Catalog, build 38
 export GTEx=~/rds/public_databases/GTEx/csv
 export eQTLCatalogue=~/rds/public_databases/eQTLCatalogue
-
-gunzip -c $GTEx/Whole_Blood.tsv.gz | head -1 > ${HbF}/work/eQTL.tsv
 Rscript -e '
   options(width=200)
   GTEx <- Sys.getenv("GTEx")
@@ -226,24 +221,34 @@ Rscript -e '
   all_files <- c(file.path(GTEx,paste0(GTEx_tissues,".tsv.gz")),file.path(eQTLCatalogue,paste0(eQTLCatalogue_tissues,".all.tsv.gz")))
   all_names <- c(GTEx_tissues,eQTLCatalogue_tissues)
   all_indices <- 1:length(all_names)
-  write.table(cbind(all_indices,all_names,all_files),row.names=FALSE,col.names=FALSE,quote=FALSE)
-' | \
-parallel -C' ' --env HbF '
-  echo {1} {2} {3}
-  sed "1d" ${HbF}/work/hbf_GWAS_top_snps_long.txt | cut -f1,4,5,10 | \
-  awk "{gsub(/chr|_[A-Z]*/,\"\",\$1);split(\$1,a,\":\");\$1=\$1\"-\"a[2]};1" | \
-  while read -r region rsid gene ensGene
-  do
-     tabix {3} ${region} | awk -vensGene=${ensGene} "index(\$1,ensGene)||index(\$4,ensGene)"
-  done
-' >> ${HbF}/work/eQTL.tsv
+  write.table(cbind(all_indices,all_names,all_files),file=file.path(HbF,"work","eQTL.txt"),row.names=FALSE,col.names=FALSE,quote=FALSE)
+'
+cat <(gunzip -c $GTEx/Whole_Blood.tsv.gz | head -1 | awk -v OFS="\t" '{print "rsid","snpid","Gene","id",$0}') \
+    <(
+       sed '1d' ${HbF}/work/hbf_hits.txt | cut -f1,3,4,12,13 | grep -v -w NA | sed 's/, /;/g' | \
+       while read -r chr pos rsid snpid gene
+       do
+          export chr=${chr}
+          export pos=${pos}
+          export rsid=${rsid}
+          export snpid=${snpid}
+          export gene=${gene}
+          export region=$(awk -vchr=${chr} -vpos=${pos} -vM=${M} 'BEGIN{print chr":"pos-M"-"pos+M}')
+          cat ${HbF}/work/eQTL.txt | \
+          parallel -C' ' -j15 --env rsid --env snpid --env gene --env region '
+             tabix {3} ${region} | \
+             awk -v rsid=${rsid} -v snpid=${snpid} -v gene=${gene} -v id={2} -v OFS="\t" "
+                 \$3<=1e-5{print rsid,snpid,gene,id,\$0}
+             "
+          '
+        done
+      ) > ${HbF}/work/eQTL.tsv
 Rscript -e '
   options(width=200)
   HbF <- Sys.getenv("HbF")
   suppressMessages(library(dplyr))
-  eQTL <- read.delim(file.path(HbF,"work","eQTL.tsv"))
-  results <- filter(eQTL,!is.na(pvalue) & pvalue<=0.05) %>%
+  results <- read.delim(file.path(HbF,"work","eQTL.tsv")) %>%
+             filter(!is.na(pvalue)) %>%
              select(variant,pvalue,molecular_trait_object_id,beta,se,chromosome,position,type,rsid)
   write.table(results,row.names=FALSE,quote=FALSE,sep="\t")
 '
-awk '$3<1e-5' ${HbF}/work/eQTL.tsv | grep -v gz
