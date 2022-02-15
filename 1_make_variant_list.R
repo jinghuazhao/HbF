cfbf15c12e81b95a99c397313fc084fe9058d508
Jing Hua Zhao <jinghuazhao@hotmail.com>
2022-02-15 09:09:19 +0000

    HbF

--- 1_make_variant_list.R
+++ 1_make_variant_list.R
@@ -7,10 +7,10 @@ require(openxlsx)
 suppressMessages(require(dplyr))
 suppressMessages(require(gap))
 require(tidyr)
-hbf_dir <- "/rds/project/jmmh2/rds-jmmh2-projects/olink_proteomics/scallop/HbF"
-hbf_xlsx <- "hbf_GWAS_top_snps.xlsx"
-if(!dir.exists(file.path(hbf_dir,"work"))) dir.create(file.path(hbf_dir,"work"))
-hbf_hits <- read.xlsx(file.path(hbf_dir,hbf_xlsx),sheet=1,colNames=TRUE,startRow=1,skipEmptyRows=TRUE) %>%
+HbF <- "/rds/project/jmmh2/rds-jmmh2-projects/olink_proteomics/scallop/HbF"
+hbf_xlsx <- file.path(HbF,"doc","hbf_GWAS_top_snps.xlsx")
+if(!dir.exists(file.path(HbF,"work"))) dir.create(file.path(HbF,"work"))
+hbf_hits <- read.xlsx(file.path(HbF,hbf_xlsx),sheet=1,colNames=TRUE,startRow=1,skipEmptyRows=TRUE) %>%
             rename(gene="Candidate.gene(s)",b=X9,p.value="p-value") %>%
             mutate(p=pvalue(b/SE),seqnames=paste0("chr",CHR),start=BP,end=BP)
 # only as a reference, chr2:60718043_G_T rs1427407 indicates that it is in GRCh37/hg19 format
@@ -46,8 +46,8 @@ hbf_hits_long <- left_join(hbf_hits_lifted,data.frame(pQTLtools::hg19Tables),by=
 # a more useable form
 data.frame(hbf_hits_long)
 
-write.table(select(hbf_hits,-seqnames,-start,-end),file=file.path(hbf_dir,"work","hbf_GWAS_top_snps.txt"),row.names=FALSE,quote=FALSE,sep="\t")
-write.table(hbf_hits_long,file=file.path(hbf_dir,"work","hbf_GWAS_top_snps_long.txt"),row.names=FALSE,quote=FALSE,sep="\t")
+write.table(select(hbf_hits,-seqnames,-start,-end),file=file.path(HbF,"work","hbf_GWAS_top_snps.txt"),row.names=FALSE,quote=FALSE,sep="\t")
+write.table(hbf_hits_long,file=file.path(HbF,"work","hbf_GWAS_top_snps_long.txt"),row.names=FALSE,quote=FALSE,sep="\t")
 
 hbf_hits_region <- liftRegion(select(hbf_hits,seqnames,start,end,REF,ALT),chain) %>%
                    mutate(snpid=chr_pos_a1_a2(seqnames,start,REF,ALT,prefix=""),
@@ -56,4 +56,4 @@ hbf_hits_region <- liftRegion(select(hbf_hits,seqnames,start,end,REF,ALT),chain)
                    mutate(pos38=start38) %>%
                    arrange(CHR,BP) %>%
                    select(CHR,BP,pos38,rs.ID,,REF,ALT,b,SE,p,Locus,snpid,snpid38,gene)
-write.table(hbf_hits_region,file=file.path(hbf_dir,"work","hbf_hits.txt"),row.names=FALSE,quote=FALSE,sep="\t")
+write.table(hbf_hits_region,file=file.path(HbF,"work","hbf_hits.txt"),row.names=FALSE,quote=FALSE,sep="\t")
